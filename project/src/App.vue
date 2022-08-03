20b0b869ffb384fb9c0e28c828d11e905948247b
eunjiP <dae4227@naver.com>
2022-08-03 16:19:39 +0900

    글꼴 추가

--- project/src/App.vue
+++ project/src/App.vue
@@ -14,5 +14,19 @@ export default {
 </script>
 
 <style>
-
+@font-face {
+    font-family: 'round';
+    src: url('../src/assets/font/Cafe24Ssurround.ttf') format('truetype');
+}
+@font-face {
+    font-family: 'roundAir';
+    src: url('../src/assets/font/Cafe24SsurroundAir.ttf') format('truetype');
+}
+  * {
+    margin: 0;
+    padding: 0;
+    border-collapse: collapse;
+    background: #152A35;
+    font-family: roundAir;
+  }
 </style>
--- project/src/components/mainHeader.vue
+++ project/src/components/mainHeader.vue
@@ -6,15 +6,15 @@
         <div>마이페이지</div>
       </div>
       <div class="header__logo">
-        <img src="../assets/logo.png">
+        <img src="../assets/img/logo.png">
       </div>
       <div class="header__right">
         <div class="header__search">
-            <form action="" method="post">
+            <form action="#" method="post">
                 <input type="text" name="search" placeholder="Search">
                 <input type="submit" value="🔍">
             </form>
-            <div>상세검색</div>
+            <div class="search__bottom">상세검색</div>
         </div>
       </div>
   </header>
@@ -28,11 +28,13 @@ export default {
 
 <style scoped>
   header {
+    margin: 1% 5% 0 5%;
     display: grid;
     grid-template-columns: 1fr 2fr 1fr;
+    color: #fff;
   }
   .header__left {
-    margin-left: 10%;
+    margin-top: 15px;
     display: flex;
     justify-content: space-around;
   }
@@ -42,12 +44,15 @@ export default {
   .header__logo > img {
     width: 8rem;
   }
-  .header__right {
-    margin-right: 10%;
-  }
   .header__search {
+    margin-top: 15px;
     display: grid;
     grid-template-rows: 1fr 1fr;
     text-align:end ;
+    line-height: 2rem;
+  }
+  .header__search input[type=text] {
+    height: 1.5rem;
+    background: #fff;
   }
 </style>
