fa5ced78c1b87c3771a51af899428b4c7551c559
eunjiP <dae4227@naver.com>
2022-08-03 18:10:00 +0900

    modal 수정

--- project/src/main.js
+++ project/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import router from './router'
 
