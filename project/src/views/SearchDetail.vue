83b56a37bbcbb1b9dbd164a14a00da2d10ef5616
yeongrong-heo <yeongr1203@gmail.com>
2022-08-04 21:07:40 +0900

    search_start

---
+++ project/src/views/SearchDetail.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>영화</div>
+        <div>
+            <img src="">
+            <div>영화 정보</div>
+        </div>
+    </div>
+    <div>
+        <div>예고편</div>
+        <div></div>
+    </div>
+    <div>
+        <div>예매시간</div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+
+</style>
+
