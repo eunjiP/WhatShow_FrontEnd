99fcc75cf57e22f91191c742196b916b257fa769
gaeng-sigi <elvin2@naver.com>
2022-08-08 12:12:45 +0900

    상세검색 수정

--- project/src/components/mainHeader.vue
+++ project/src/components/mainHeader.vue
@@ -97,7 +97,7 @@
             <div class="row">
               <div>
                 <label v-for="item in gsTag" :key="item" class="col-3">
-                  <input type="checkbox" name="genre" >{{ item }}
+                  <input type="checkbox" name="genre" > {{ item }}
                 </label>
               </div>
             </div>
@@ -228,6 +228,7 @@
     background-color: #F29B21;
     border: none;
     border-radius: 10px;
+    padding: 5px;
     color: #fff;
   }
 
