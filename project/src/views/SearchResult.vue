45c1217e8063c536c970090a3193e50900b622c8
yeongrong-heo <yeongr1203@gmail.com>
2022-08-08 12:42:28 +0900

    수정

--- project/src/views/SearchResult.vue
+++ project/src/views/SearchResult.vue
@@ -5,16 +5,43 @@
 </template>
 
 <script>
-// const axios = require("axios");
-const axios = await axios.get("https://openapi.naver.com/v1/search/movie.json");
-
-axios.get(URL, {
-    Headers: {
-
-    }
-})
-
 export default ({
+data() {
+    return {
+        SearchMovie: []
+    }
+},
+created() {
+    const axios = await axios.get("https://openapi.naver.com/");
+},
+mounted() {
+    this.getSearchMovie();
+},
+methods: {
+    getSearchMovie: function() {
+        let search = '미니언즈';
+        const URL = "/v1/search/movie.json?query="+`${search}`+"&section=all&ie=utf8"; /*URL*/
+        const clientId = 'd68UHq382cT8ZrsbFZYj';
+        const clientSecret = 'ugKvj9QWb1';
+        this.$axios.get(URL, {
+            Headers: {
+                'Accept' : 'application/json',
+                'X-Naver-Client-Id': clientId,
+                'X-Naver-Client-Secret': clientSecret
+            }
+        }).then((response) => { 
+                console.log(response.data);
+                let List = [];
+                List = List.concat(response.data.items); 
+                this.SearchMovie = this.SearchMovie.concat(List);
+        })
+    }
+}
+// const axios = require("axios");
+
+
+
+
     // data () {
     //   return {
     //     SearchMovie : []
