710f2da337f5c298fb210ea56c67b50a1ae41574
yeongrong-heo <yeongr1203@gmail.com>
2022-08-08 16:51:45 +0900

    오후

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import main from '../views/main.vue'
 import MovieDetail from '../views/MovieDetail.vue'
+// import SearchResult from '../views/SearchResult.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'detail',
     component: MovieDetail
   },
+  // {
+  //   path: '/SearchResult',
+  //   name: 'SearchResult',
+  //   component: SearchResult
+  // }
 ]
 
 const router = createRouter({
