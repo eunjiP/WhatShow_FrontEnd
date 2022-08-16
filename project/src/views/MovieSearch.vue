cab4f29cfbe4dccce946dd86a92f0873802b080d
eunjiP <dae4227@naver.com>
2022-08-16 18:55:07 +0900

    검색 결과 프론트

--- project/src/views/MovieSearch.vue
+++ project/src/views/MovieSearch.vue
@@ -6,56 +6,49 @@
             </div>
             <div v-if="this.keyword == ''"></div>
             <div v-else>
-                <div id="search__subTitle">
-                    <div id="sub1"> 영화</div>
-                    <div></div>
-                    <div id="sub2" @click="more" style="cursor:pointer;">더보기</div>
+                <div class="search__subTitle">
+                    <div class="sub1"> 영화</div>
+                    <div class="sub2" @click="more" style="cursor:pointer;">더보기</div>
                 </div>
-                <br>
-                <div id="search-body">
-                    <br>
+                <div class="search-body m-3">
                     <div class="container">
-                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
+                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4" id="keywordResult">
                             <div class="movie__poster col" v-for="(item, idx) in movie_info" :key="idx" :item="item">
-                                <img :src="`${item.movie_poster}`"/>
-                                <div>{{ item.movie_nm }}</div>
+                                <img :src="`${item.movie_poster}`" class="mt-3"/>
+                                <div class="mb-3">{{ item.movie_nm }}</div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div></div>
             <div>
-                <div id="search__subTitle">
-                    <div id="sub1"> 검색 장르 영화</div>
-                    <div></div>
-                    <div id="sub2" @click="more" style="cursor:pointer;">더보기</div>
+                <div class="search__subTitle">
+                    <div class="sub1"> 검색 장르 영화</div>
+                    <div class="sub2" @click="more" style="cursor:pointer;">더보기</div>
                 </div>
-                <br>
-                <div id="search-body">
-                    <br>
+                <div class="search-body m-3">
+                    <div class="container">
+                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4" id="genreResult">
+                            <div class="movie__poster col" v-for="(item, idx) in movie_info2" :key="idx" :item="item">
+                                <img :src="`${item.movie_poster}`" class="mt-3"/>
+                                <div class="mb-3">{{ item.movie_nm }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="search__subTitle">
+                    <div class="sub1"> # 태그 추천</div>
+                    <div class="sub2">더보기</div>
+                </div>
+                <div class="search-body m-3">
                     <div class="container">
                         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
-                            <div class="movie__poster col" v-for="(item, idx) in movie_info2" :key="idx" :item="item">
-                                <img :src="`${item.movie_poster}`"/>
-                                <div>{{ item.movie_nm }}</div>
+                            <div class="movie__poster col" v-for="(item, idx) in movie_recommend_info" :key="idx" :item="item">
+                                <img :src="`${item.movie_poster}`" class="mt-3"/>
+                                <div class="mb-3">{{ item.movie_nm }}</div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div id="search__subTitle">
-                    <div id="sub1"> # 태그 추천</div>
-                    <div></div>
-                    <div id="sub2">더보기</div>
-                </div>
-                <div id="search-body">
-                    <br>
-                    <div class="movie__poster row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
-                        <div class="movie__poster col" v-for="(item, idx) in movie_recommend_info" :key="idx" :item="item">
-                            <img :src="`${item.movie_poster}`"/>
-                            <div>{{ item.movie_nm }}</div>
                         </div>
                     </div>
                 </div>
@@ -72,6 +65,7 @@ export default {
         return {
             movielimit: 4,
             movie_info: [],
+            movie_info2: [],
             movie_recommend_info: [],
             movie_tag: [],
             keyword: this.$route.params.keyword || '',  //mainHeader.vue에서 라우터를 이용해 보낸 파라미터로부터 데이터 받음 
@@ -81,7 +75,7 @@ export default {
 
     created() {
         this.getMovieInfo(); // 영화 상세 정보
-        // this.getMovieInfoTag();
+        this.getMovieInfoTag();
         this.getRecommendMovie();
     },
 
@@ -93,28 +87,40 @@ export default {
             this.$store.commit('increment');
         },
 
+        async getMovieInfo() {
+            const param = {
+                'keyword': this.keyword
+            };
+            this.movie_info = await this.$get('recommend/movieSearch', param);
+            if (this.movie_info.length === 0) {
+                const keywordResult = document.querySelector('#keywordResult');
+                keywordResult.innerHTML = "<div class='resultNull'>검색 결과가 없습니다!</div>";
+            } else {
+                console.log(this.movie_info);
+            }
+        },
+
         async getRecommendMovie() {
             const param = {
                 'iuser': parseInt(localStorage.getItem('iuser'))
             };
             console.log(param);
             this.movie_recommend_info = await this.$get('recommend/tagRecommend', param);
-            console.log(this.movie_recommend_info);
+            // console.log(this.movie_recommend_info);
         },
 
         async getMovieInfoTag() { // 영화 상세 정보
-            const data = {
-                'tag':this.$route.params.keyTag
+            const param = {
+                'keyword': this.keyword
             };
-            this.movie_info2 = await this.$post(`/recommend/tagRecommend`, data);
-            console.log(this.movie_info);
-        },
-
-        async getMovieInfo() { // 영화 상세 정보
-        console.log(this.$route.params.keyTag);
-            // console.log(`req : ${this.$route.params.keyTag}`);
-            // this.movie_info = await this.$get(`/movie/selSearch/${this.keyword}/${this.movielimit}`, {});
-            // console.log(this.movie_info);
+            this.movie_info2 = await this.$get(`/recommend/movieTagSearch`, param);
+            if (this.movie_info2.length === 0) {
+                const genreResult = document.querySelector('#genreResult');
+                genreResult.innerHTML = "<div class='resultNull'>검색 결과가 없습니다!</div>";
+            } else {
+                console.log(this.movie_info);
+            }
+            console.log(this.movie_info2);
         },
 
         more() { // 검색 결과 더보기
@@ -140,7 +146,7 @@ export default {
     }
 
     /* 소제목 타이틀 */
-    #search__subTitle {
+    .search__subTitle {
         width: 100%;
         display: inline-block;
         vertical-align: middle;
@@ -150,7 +156,7 @@ export default {
     }
 
     /* 소제목 */
-    #sub1 {
+    .sub1 {
         float: left;
         font-size: 1.5rem;
         color: var(--font--color);
@@ -158,7 +164,7 @@ export default {
     }
 
     /* 더보기 */
-    #sub2 {
+    .sub2 {
         float: right;
 
     }
@@ -167,4 +173,5 @@ export default {
         text-align: center;
 
     }
+
 </style>
