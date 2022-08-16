0127ef8654787de99f2e7c77b4b975f1e95e4be4
gaeng-sigi <elvin2@naver.com>
2022-08-16 20:24:27 +0900

    footer

---
+++ project/src/components/mainFooter.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer>
+        <div class="main__footer">
+            <hr>
+            <div>팀명 : BOAAA(혈액형)</div>
+            <div>팀장 : 황현</div>
+            <div>팀원 : 허영롱 박은지 황영은 이경식</div>
+            <div>주소 : 대구 중구 중앙대로 394 제일빌딩 5F, 506호</div>
+            <div>전화번호 : 053-572-1005</div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .main__footer {
+        margin: 0 100px 0 100px;
+        color: white;
+    }
+
+    body::-webkit-scrollbar { display:none; }
+</style>
